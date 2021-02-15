a6d9f177e70eff57287707a24f0dcfb2f1e09b54
gabzafra <gab.zafra@gmail.com>
2021-02-15 15:00:10 +0100

    addent objects to keep data

--- part1/information/src/index.js
+++ part1/information/src/index.js
@@ -5,9 +5,9 @@ const Header = (props) => <h1>{props.course}</h1>;
 
 const Content = (props) => (
   <>
-    <Part name={props.part1} number={props.exercises1} />
-    <Part name={props.part2} number={props.exercises2} />
-    <Part name={props.part3} number={props.exercises3} />
+    <Part content={props.part1} />
+    <Part content={props.part2} />
+    <Part content={props.part3} />
   </>
 );
 const Total = (props) => (
@@ -18,34 +18,37 @@ const Total = (props) => (
 
 const Part = (props) => (
   <p>
-    {props.name} {props.number}
+    {props.content.name} {props.content.exercises}
   </p>
 );
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14
+  };
 
   return (
     <div>
       <Header course={course} />
       <Content
         part1={part1}
-        exercises1={exercises1}
         part2={part2}
-        exercises2={exercises2}
         part3={part3}
-        exercises3={exercises3}
       />
       <Total
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
+        exercises1={part1.exercises}
+        exercises2={part2.exercises}
+        exercises3={part3.exercises}
       />
     </div>
   );
