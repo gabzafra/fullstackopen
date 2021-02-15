36124bd7ccbb2582d2ca6d53ee26cc90b3e09628
gabzafra <gab.zafra@gmail.com>
2021-02-15 14:30:11 +0100

    Added "Part" component

--- part1/information/src/index.js
+++ part1/information/src/index.js
@@ -5,20 +5,20 @@ const Header = (props) => <h1>{props.course}</h1>;
 
 const Content = (props) => (
   <>
-    <p>
-      {props.part1} {props.exercises1}
-    </p>
-    <p>
-      {props.part2} {props.exercises2}
-    </p>
-    <p>
-      {props.part3} {props.exercises3}
-    </p>
+    <Part name={props.part1} number={props.exercises1} />
+    <Part name={props.part2} number={props.exercises2} />
+    <Part name={props.part3} number={props.exercises3} />
   </>
 );
 const Total = (props) => (
   <p>
     Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+  </p>
+);
+
+const Part = (props) => (
+  <p>
+    {props.name} {props.number}
   </p>
 );
 
