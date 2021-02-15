4a10997b0747986588d350cea2e8c4e2c3fd18eb
gabzafra <gab.zafra@gmail.com>
2021-02-15 15:04:33 +0100

    objects array

--- part1/information/src/index.js
+++ part1/information/src/index.js
@@ -5,9 +5,9 @@ const Header = (props) => <h1>{props.course}</h1>;
 
 const Content = (props) => (
   <>
-    <Part content={props.part1} />
-    <Part content={props.part2} />
-    <Part content={props.part3} />
+    <Part content={props.parts[0]} />
+    <Part content={props.parts[1]} />
+    <Part content={props.parts[2]} />
   </>
 );
 const Total = (props) => (
@@ -24,31 +24,28 @@ const Part = (props) => (
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14
-  };
-
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
   return (
     <div>
       <Header course={course} />
-      <Content
-        part1={part1}
-        part2={part2}
-        part3={part3}
-      />
+      <Content parts={parts} />
       <Total
-        exercises1={part1.exercises}
-        exercises2={part2.exercises}
-        exercises3={part3.exercises}
+        exercises1={parts[0].exercises}
+        exercises2={parts[1].exercises}
+        exercises3={parts[2].exercises}
       />
     </div>
   );
