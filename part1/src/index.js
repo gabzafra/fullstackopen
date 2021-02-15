5d664528982b937d681c00bdb7d30dd19ab75a7a
gabzafra <gab.zafra@gmail.com>
2021-02-15 13:58:20 +0100

    end lesson

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  console.log("Hello from component");
+const Hello = (props) => {
   return (
     <div>
-      <p>Hello world</p>
+      <p>Hello {props.name}, you are {props.age}</p>
+    </div>
+  );
+};
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
     </div>
   );
 };
