c3a916a6f701cc91b9b8b2fae76c5f9468835e44
gabzafra <gab.zafra@gmail.com>
2021-02-16 11:19:27 +0100

    button counter

--- part1/src/index.js
+++ part1/src/index.js
@@ -2,16 +2,12 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
-
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
+  const [counter, setCounter] = useState(0);
 
   return (
     <div>
-      {counter}
+      <div>{counter}</div>
+      <button onClick={() => setCounter(counter + 1)}>plus</button>
     </div>
   );
 };
