418415dbee3e0afdd96991b59fa396ec0a3d5679
gabzafra <gab.zafra@gmail.com>
2021-02-16 10:48:30 +0100

    re-rendering

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,29 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age;
-
+const App = (props) => {
+  const {counter} = props
   return (
     <div>
-      <p>
-        Hello {name}, you are {age}
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
+      {counter}
     </div>
   );
 };
 
-const App = () => {
-  const name = "Peter";
-  const age = 10;
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-    </div>
-  );
-};
+let counter = 1
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App counter={counter}/>, document.getElementById("root"));
