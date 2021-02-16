10d95c3ad64a7a06ea7f6378b780f8fe08b8de42
gabzafra <gab.zafra@gmail.com>
2021-02-16 11:37:05 +0100

    destructuring

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,13 +1,11 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const Display = (props) => {
-  return <div>{props.counter}</div>;
-};
+const Display = ({ counter }) => <div>{counter}</div>;
 
-const Button = (props) => {
-  return <button onClick={props.handleClick}>{props.text}</button>;
-};
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
 
 const App = () => {
   const [counter, setCounter] = useState(0);
