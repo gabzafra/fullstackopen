75eb5edc752b14e7d02810225bbbfff2af2cc640
gabzafra <gab.zafra@gmail.com>
2021-02-16 10:46:15 +0100

    Destructuring

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,17 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Hello = (props) => {
+const Hello = ({ name, age }) => {
+  const bornYear = () => new Date().getFullYear() - age;
+
   return (
     <div>
-      <p>Hello {props.name}, you are {props.age}</p>
+      <p>
+        Hello {name}, you are {age}
+      </p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   );
 };
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  const name = "Peter";
+  const age = 10;
   return (
     <div>
       <h1>Greetings</h1>
