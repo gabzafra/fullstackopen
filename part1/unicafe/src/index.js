6ea7a5a12c18536e48e06abd5e862edaa05fb2e7
gabzafra <gab.zafra@gmail.com>
2021-02-16 13:21:12 +0100

    unicafe step 1

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,12 +1,49 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const Scoreboard = ({ score, text }) => {
+  return (
+    <p>
+      {text} {score}
+    </p>
+  );
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  const addFeedback = (feedback) => {
+    switch (feedback) {
+      case "good":
+        return () => setGood(good + 1);
+        break;
+      case "neutral":
+        return () => setNeutral(neutral + 1);
+        break;
+      case "bad":
+        return () => setBad(bad + 1);
+        break;
+    }
+  };
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={addFeedback("good")} text="good" />
+      <Button handleClick={addFeedback("neutral")} text="neutral" />
+      <Button handleClick={addFeedback("bad")} text="bad" />
+      <h1>statistics</h1>
+      <Scoreboard score={good} text="good" />
+      <Scoreboard score={neutral} text="neutral" />
+      <Scoreboard score={bad} text="bad" />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
