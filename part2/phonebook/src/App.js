f24cd872f85f00d6dfad7e7a2ef52b151b27f745
gabzafra <gab.zafra@gmail.com>
2021-02-18 10:47:36 +0100

    phonebook step 4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,14 @@ import React, { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: 6524104 },
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [newSearch, setNewSearch] = useState("");
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -30,10 +34,17 @@ const App = () => {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
   };
+  const handleSearchChange = (event) => {
+    setNewSearch(event.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <p>
+        filter show with{" "}
+        <input value={newSearch} onChange={handleSearchChange}></input>
+      </p>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -48,12 +59,14 @@ const App = () => {
       <h2>Numbers</h2>
       <table>
         <tbody>
-          {persons.map((person) => (
-            <tr key={person.name}>
-              <td>{person.name}</td>
-              <td>{person.number}</td>
-            </tr>
-          ))}
+          {persons
+            .filter((person) => person.name.toUpperCase().includes(newSearch.toUpperCase()))
+            .map((person) => (
+              <tr key={person.name}>
+                <td>{person.name}</td>
+                <td>{person.number}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
