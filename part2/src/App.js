d94e9f28ffd62c345299c01444539a6516f3effd
gabzafra <gab.zafra@gmail.com>
2021-02-19 08:36:54 +0100

    sending data to server

--- part2/src/App.js
+++ part2/src/App.js
@@ -8,29 +8,28 @@ const App = () => {
   const [showAll, setShowAll] = useState(true);
 
   const hook = () => {
-    console.log('effect')
     axios
       .get('http://localhost:3001/notes')
       .then(response=>{
-        console.log('promise fulfilled')
         setNotes(response.data)
       })
   }
 
   useEffect(hook, [])
 
-  console.log('render', notes.length, 'notes');
-
   const addNote = (event) => {
     event.preventDefault();
     const noteObject = {
-      id: notes.length + 1,
       content: newNote,
-      date: new Date().toISOString(),
+      date: new Date(),
       important: Math.random() < 0.5,
     };
-    setNotes(notes.concat(noteObject));
-    setNewNote("");
+    axios
+      .post('http://localhost:3001/notes', noteObject)
+      .then(response => {
+        setNotes(notes.concat(response.data))
+        setNewNote('')
+      })
   };
 
   const handleNoteChange = (event) => {
