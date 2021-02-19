5171aa6aa8671c06026a2ac4f28a936e8a340027
gabzafra <gab.zafra@gmail.com>
2021-02-19 09:43:30 +0100

    phonebook step 7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -25,9 +25,14 @@ const App = () => {
         name: newName,
         number: newNumber,
       };
-      setPersons(persons.concat(personObj));
-      setNewName("");
-      setNewNumber("");
+
+      axios
+        .post("http://localhost:3001/persons", personObj)
+        .then((response) => {
+          setPersons(persons.concat(response.data));
+          setNewName("");
+          setNewNumber("");
+        });
     } else {
       alert(`${newName} is already added to phonebook`);
     }
