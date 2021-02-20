d6b7ed98c9ccb7f631ea905e2c51f986d67c54d5
gabzafra <gab.zafra@gmail.com>
2021-02-20 12:55:54 +0100

    phonebook backend step 3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -4,27 +4,6 @@ const app = express();
 
 app.use(express.json());
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true,
-  },
-];
-
 const persons = [
   {
     name: "Arto Hellas",
@@ -55,6 +34,13 @@ app.get("/info", (request, response) => {
 app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    person ? response.json(person) : response.status(404).end()
+    
+})
 
 const PORT = 3001;
 app.listen(PORT, () => {
