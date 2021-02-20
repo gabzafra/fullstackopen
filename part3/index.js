a8af6d6655f01be4369c9c1f4261a2b6df4fcd7d
gabzafra <gab.zafra@gmail.com>
2021-02-20 10:30:25 +0100

    fetching single resource

--- part3/index.js
+++ part3/index.js
@@ -23,11 +23,18 @@ let notes = [
 ];
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello Sunny!</h1>')
+    response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
     response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+    note ? response.json(note) : response.status(404).end()
+    
 })
 
 const PORT = 3001;
