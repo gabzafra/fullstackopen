2b34ad69b11153563c057597ed52fd87dd14d2b6
gabzafra <gab.zafra@gmail.com>
2021-02-20 11:20:29 +0100

    receiving data

--- part3/index.js
+++ part3/index.js
@@ -1,5 +1,8 @@
+const { response } = require('express');
 const express = require('express')
 const app = express()
+
+app.use(express.json())
 
 let notes = [
   {
@@ -43,6 +46,33 @@ app.delete('/api/notes/:id', (request, response) => {
     response.status(204).end()
 })
 
+const generateId = () => {
+   const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+    return maxId + 1 
+}
+
+app.post('/api/notes', (request, response)=> {
+    const body = request.body
+    
+    if(!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId()
+    }
+
+    notes = notes.concat(note)
+    
+    response.json(note)
+})
 
 const PORT = 3001;
 app.listen(PORT, () => {
